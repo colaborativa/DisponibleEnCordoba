e3f286b8ba68f40f36aaa0e5bbf9d65c6b532ef9
magda <magdasanchez@colaborativa.eu>
2013-01-04 17:27:43 +0100

    Adding POINTS to Map from Google Spreadsheet automatically
    Signed-off-by: magda <magdasanchez@colaborativa.eu>

---
+++ ext/google_docs-disponibleencordoba.js
@@ -0,0 +1,76 @@
+function mmg_google_docs_spreadsheet_1(id, callback) {
+    if (typeof reqwest === 'undefined'){
+        throw 'CSV: reqwest required for mmg_csv_url';
+    }
+
+    var url = 'https://spreadsheets.google.com/feeds/list/' +
+        id + '/od6/public/values?alt=json-in-script&callback=callback';
+    reqwest({
+        url: url,
+        type: 'jsonp',
+        jsonpCallback: 'callback',
+        success: response,
+        error: response
+    }); 
+   
+    function response(x) {
+        var features = [],
+            latfield = '',
+            lonfield = '';
+        if (!x || !x.feed) return features;
+        for (var f in x.feed.entry[0]) {
+            if (f.match(/\$Lat/i)){
+                latfield = f;                    
+            }
+            if (f.match(/\$Lon/i)){
+                lonfield = f;     
+            }
+        }
+
+        for (var i = 0; i < x.feed.entry.length; i++) {                             
+            var entry = x.feed.entry[i];
+            //console.log(entry);
+            var feature = {
+                geometry: {
+                    type: 'Point',
+                    coordinates: []
+                },
+                properties: {
+                    'marker-color':'#000',
+                    'titulo': entry['gsx$titulo'].$t,
+                    'descripcion': entry['gsx$descripcion'].$t,  
+                    'estado': entry['gsx$estado'].$t,
+                    'enlace': entry['gsx$enlace'].$t,  
+                    'categoria': entry['gsx$categoria'].$t,  
+                }
+            };
+
+            for (var y in entry) {
+                if (y === latfield){
+                    feature.geometry.coordinates[1] = parseFloat(entry[y].$t);
+                    //console.log("coordinate latfield "+ feature.geometry.coordinates[1]); 
+                }
+                else if (y === lonfield) {
+                    feature.geometry.coordinates[0] = parseFloat(entry[y].$t);
+                    //console.log("coordinate lonfield "+ feature.geometry.coordinates[0]); 
+                }
+                else if (y.indexOf('gsx$') === 0) {                            
+                    feature.properties[y.replace('gsx$', '')] = entry[y].$t;
+                }
+            }
+            
+            if (feature.geometry.coordinates.length == 2){
+                 features.push(feature);
+                 //console.log("feature.geometry.coordinates"+feature.geometry.coordinates.length);
+            }
+            //console.log(features);
+           /* _.each(feature, function(value, key) {
+                if(feature.properties['categoria']=="Robo"){ feature.properties['marker-color']='#CB3344'} 
+                if(feature.properties['categoria']=="Intento de Robo") {feature.properties['marker-color']='#FFCC33'}
+                if(feature.properties['categoria']=="Agresión") { feature.properties['marker-color']='#653332'}   
+            }); */
+        }
+        return callback(features);
+    }
+}
+
--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-// Create and load map
-$('#map').mapbox('colaborativa.OSMCordoba', function(map, tilejson) {
 
+// Datos y Mapa IDs
+var map;
+var data_id = '0Asc521FZEVkpdFNEYl9UTnNkV0FOdXdEME9keVhnanc';
+var map_id = 'colaborativa.OSMCordoba';
+// Extracting Data from Google Drive, on finished call mapData function
+mmg_google_docs_spreadsheet_1(data_id, mapData );
+// Create and set Map
+$('#map').mapbox('colaborativa.OSMCordoba', function(mapTemp, tilejson) {
+    map = mapTemp;
     map.setZoomRange(14, 16);
     map.centerzoom({ lat: 37.885, lon: -4.79 }, 14);
     map.setPanLimits([{ lat: 37.9189, lon: -4.8339 }, { lat: 37.8455, lon: -4.7062 }]);
     map.zoom(14, true);
-    // Add share control
-
-    // Set title and description from tilejson
-    // override for disponibleEnCordoba
-    /* document.title = tilejson.name;
-    $('h1.map-title').text(tilejson.name);
-    $('p.description').text(tilejson.description); */
-
-
-    var container = $('#markerfilters');
-    $.each(tilejson.markers.markers(), function(index, m) {
-        var s = m.data.properties['marker-symbol'];
-
-        if (container.find('[href="#' + s + '"]').length) return;
-
-        var el = $(document.createElement('a'))
-            .addClass('markerfilter')
-            .attr('href', '#' + s)
-            .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-'+s+'+000000.png)')
-            .bind('click', filter);
-        container.append(el);
-    });
-
-
-    $('[href="#all"]').bind('click', filter);
-
-
-    function filter(e) {
-        container.find('a').removeClass('selected');
-        var id = $(this).addClass('selected').attr('href').replace('#', '');
-        tilejson.markers.filter(function(feature) {
-            return feature.properties['marker-symbol'] == id || id == 'all';
-        });
-        return false;
-    }
 });
+// Load points
+function mapData(f) {
+    features = f;
+    // Adding points to map
+    markerLayer = mapbox.markers.layer().features(features);
+    // Adding interaction layer
+    interaction = mapbox.markers.interaction(markerLayer);
+    map.addLayer(markerLayer);
+    // Defining interactive layer
+    interaction.formatter(function (feature) {
+        var o = '<h3>' + feature.properties.titulo + '</h3>' +
+            '<img src="' + feature.properties.enlace + '" alt="' + feature.properties.titulo + '">' +
+            '<p>' + feature.properties.descripcion + '</p>';
+        return o;
+    });
+    download_data();
+}
+/*
+Function for put href for download data
+URL Spreadsheet #DisponibleEnCordoba
+https://docs.google.com/spreadsheet/pub?key=0Asc521FZEVkpdFNEYl9UTnNkV0FOdXdEME9keVhnanc&single=true&gid=0&output=csv
+*/
+function download_data() {
+    $('#download_csv').attr('href', 'https://docs.google.com/spreadsheet/pub?key=' + data_id + '&output=csv');
+    $('#download_json').attr('href', 'https://spreadsheets.google.com/feeds/list/' + data_id + '/od6/public/values?alt=json-in-script');
+}
+
+
